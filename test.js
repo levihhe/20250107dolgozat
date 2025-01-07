74c6f8bc20b570ebf3ef39d7f4a366e93af34e79
Csóré Levente <103035763+levihhe@users.noreply.github.com>
2025-01-07 13:20:13 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,42 @@
+function getCount(str) {
+    let count = 0;
+    for (let i = 0;i<str.length;i++) {
+      if(str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u") {
+        count++;
+      }
+    }
+    return count;
+}
+
+function divisors(integer) {
+    let count = 0;
+    let tomb = [];
+    for(let i = 2;i<integer;i++) {
+      if (integer % i == 0) {
+        count++;
+        tomb.push(i);
+      }
+    }
+    if (count!=0) {
+        return tomb
+    }
+    else {
+        return integer+" is prime"
+    }
+}
+
+function filter_list(l) {
+    var res = [];
+    for(var i=0;i<l.length;i++) {
+      if (typeof l[i] == "number") {
+        res.push(l[i])
+      }
+    }
+    return res;
+}
+
+function hasUniqueChars(str){
+    return str.split('').every(function(v, i, arr){
+      return arr.indexOf(v) == i;
+    });
+}
